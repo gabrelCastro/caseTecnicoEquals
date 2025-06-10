197282300d10f090ef8ded7f38a297eae583b3fb
Gabriel de Castro <gf-castro2012@hotmail.com>
2025-06-10 15:10:16 -0300

    :wrench: chore: Adicionando arquivo do initializr

---
+++ equalscase/src/main/java/com/gabriel/equalscase/EqualscaseApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.equalscase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EqualscaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EqualscaseApplication.class, args);
+	}
+
+}
